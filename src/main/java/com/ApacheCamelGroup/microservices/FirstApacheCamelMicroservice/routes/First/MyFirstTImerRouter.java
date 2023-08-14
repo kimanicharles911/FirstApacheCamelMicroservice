fd9d9351751557970d3525a3511b43ad5fe4b91f
kimanicharles911 <kimanicharles911@gmail.com>
2023-08-14 17:28:46 +0300

    processing route operation: using a beans logger

--- src/main/java/com/ApacheCamelGroup/microservices/FirstApacheCamelMicroservice/routes/First/MyFirstTImerRouter.java
+++ src/main/java/com/ApacheCamelGroup/microservices/FirstApacheCamelMicroservice/routes/First/MyFirstTImerRouter.java
@@ -3,6 +3,8 @@ package com.ApacheCamelGroup.microservices.FirstApacheCamelMicroservice.routes.F
 import java.time.LocalDateTime;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +13,23 @@ public class MyFirstTImerRouter extends RouteBuilder {
 	
 	@Autowired
 	private GetCurrentTimeBean getCurrentTimeBean;
+	
+	@Autowired
+	private SimpleLoggingProcessingComponent loggingProcessingComponent;
 
 	@Override
 	public void configure() throws Exception {
 		// TODO Auto-generated method stub
 		//queue -  listening to a queue & its input (for tutorial purposes we will utilise a timer)
 		//transformation - making changes to the message that comes in from the queue 
-		//database - save the tranformed message ( for tutorial purposes we will utilise logs)
+		//database - save the transformed message ( for tutorial purposes we will utilise logs)
 		from("timer:first-timer")
+		
 		.bean(getCurrentTimeBean)
+		.log("${body}")
+		// Route Ops - Processing & Transformation
+		.bean(loggingProcessingComponent)
+		.log("${body}")
 		.to("log:first-timer");
 	}
 	
@@ -32,13 +42,14 @@ class GetCurrentTimeBean {
   }
 }
 
-
-
-
-
-
-
-
-
+@Component
+class SimpleLoggingProcessingComponent {
+	
+	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+	// Using a bean process with void mean we are doing some kind of processing in here.
+	public void process(String message) {
+		logger.info("SimpleLoggingProcessingComponent {}", message);
+	}
+}
 
 
