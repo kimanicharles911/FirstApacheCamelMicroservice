ac8ae1a7cb0a2b114ff3dd7e2f557b652fa4c4bd
kimanicharles911 <kimanicharles911@gmail.com>
2023-08-14 16:03:44 +0300

    Beans: using beans to tranform a message

--- src/main/java/com/ApacheCamelGroup/microservices/FirstApacheCamelMicroservice/routes/First/MyFirstTImerRouter.java
+++ src/main/java/com/ApacheCamelGroup/microservices/FirstApacheCamelMicroservice/routes/First/MyFirstTImerRouter.java
@@ -1,5 +1,7 @@
 package com.ApacheCamelGroup.microservices.FirstApacheCamelMicroservice.routes.First;
 
+import java.time.LocalDateTime;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +15,17 @@ public class MyFirstTImerRouter extends RouteBuilder {
 		//transformation - making changes to the message that comes in from the queue 
 		//database - save the tranformed message ( for tutorial purposes we will utilise logs)
 		from("timer:first-timer")
-		.transform().constant("My constant message")
+		.bean("getCurrentTimeBean")
 		.to("log:first-timer");
 	}
 	
+}
+
+@Component
+class GetCurrentTimeBean {
+  public String getCurrentTime() {
+	  return "issa Holiday => " + LocalDateTime.now();
+  }
 }
 
 
